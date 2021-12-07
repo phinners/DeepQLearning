399a4b26e9a32c2d89ea9cc467f6aebded0e9009
Patrick <patrick.hinners@iubh.de>
2021-12-07 18:50:43 +0100

    Mistakes in QValue calculation and optimization in Samplebuffer

--- Agent.py
+++ Agent.py
@@ -11,6 +11,7 @@ import datetime
 from DuelingDeepQModel import DuelingDeepQModel
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
+import itertools
 
 class Agent():
     def __init__(self, Environment, Load_Weights=False, Weights_Path=""):
@@ -22,19 +23,21 @@ class Agent():
         """
         self.env_name = Environment
         self.env = gym.make(self.env_name)          # Create OpenAI Gym environment with env_name
-        self.env.reset()
+        self.env.seed(1234)
+        frame = self.env.reset()
+        tf.random.set_seed(1234)
 
         self.action_size = self.env.action_space.n  # Number of Outputs from the Neural Network
         self.shape_frame = [84, 84]                 # Shape of Input to the Neural Network
         self.num_frames = 4                         # Number of Stacked Frames for Input to Neural Network
 
         self.epsilon = 0.9                          # Propability of random action choice in epsilon greedy
-        self.epsilon_decrease = 0.005/100            # Decrease of Epsilon per Episode
+        self.epsilon_decrease = 2.5/100000 #.9/100000 #0.1/100            # Decrease of Epsilon per Episode
         self.epsilon_min = 0.05                     # Lower Bound of Epsilon Value
 
         self.gamma = 0.9
 
-        self.EPISODES = 20000                       # Number of Episodes to play
+        self.EPISODES = 120000                       # Number of Episodes to play
         self.episode_counter = 0                    # Counter of Episodes
 
         self.scores = []                            # Buffer of Scores for Visualizing the Performance
@@ -53,7 +56,7 @@ class Agent():
                                        self.shape_frame,
                                        self.action_size)
 
-        optimizer = Adam(learning_rate=1e-3)
+        optimizer = Adam(learning_rate=1e-2)
         self.target_model.compile(optimizer, loss=tf.keras.losses.Huber())
         self.online_model.compile(optimizer, loss=tf.keras.losses.Huber())
 
@@ -73,24 +76,29 @@ class Agent():
         #Initialize Sample Buffer (Experience Replay)
         self.sample_buffer = SampleBuffer()
         #Initialize state Buffer, for stacking the number of frames for input into the Model
-        self.state_buffer = deque(maxlen=self.num_frames + 1)
+        self.frame = self.resize_observation(frame)
+        self.state_buffer = np.repeat(self.frame,  self.num_frames + 1, axis=2)
         #Initialize reward Buffer for Calculation purpose
         self.reward_buffer = deque(maxlen=2)
+        self.action_buffer = deque(maxlen=2)
+        for i in range(2):
+            self.reward_buffer.append(0)
+            self.action_buffer.append(0)
 
         self.figurename = "Results" #plt.figure("Results")
 
-    def resize_observation(self, observation):
+    def resize_observation(self, observation, shape = (84,84)):
         """
         Resize the return of the environment to the predeterined size
         :param observation: Return of the environment after taking an action
         :return: Gives the resized observation back
         """
-        resized = cv2.resize(observation, (84, 84))
-        grayscale = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
-        binary = cv2.threshold(grayscale, 140, 255, cv2.THRESH_BINARY)[1]
 
-        #resized = cv2.resize(grayscale, (84, 84))
-        return binary
+        grayscale = cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
+        resized = cv2.resize(grayscale, (84, 84), interpolation=cv2.INTER_NEAREST)
+        resized = resized.reshape((*shape, 1))
+        resized = resized.astype(np.float32)
+        return resized
 
     def policy(self, epsilon, state):
         """
@@ -99,20 +107,20 @@ class Agent():
         :param state: Input into the Model for action choice
         :return: Gives back the action to take
         """
+        #print("Epsilon: " + str(epsilon))
         if np.random.rand() < epsilon:
             #print("Random")
             return np.random.randint(self.action_size)
         else:
-            #print("Not Random")
-            # print("Q Value Action")
-            # obtain the current best Q values from the DeepQ network
-            array = np.moveaxis(np.asarray(state), [0], [2])
-            array = array.reshape(-1, array.shape[0], array.shape[1], array.shape[2])
-            tensor = tf.convert_to_tensor(array, dtype=tf.float32)
-            Q_values = self.online_model.predict(tensor)
+            # obtain the current best Q values from the DeepQ Network
+            Q_values = self.online_model.predict(state.reshape(-1,
+                                                               self.shape_frame[0],
+                                                               self.shape_frame[1],
+                                                               self.num_frames))
             # take the action that results in the highest Q value
+            return_value = np.argmax(Q_values[0])
             #print(Q_values)
-            return np.argmax(Q_values[0])
+            return return_value
 
     def play_episode(self):
         """
@@ -124,18 +132,21 @@ class Agent():
         self.env.reset()
         self.make_initial_steps(22)
         while(1):
-            if len(self.state_buffer) != 4:
+            if len(self.state_buffer) <= 4:
                 action = self.env.action_space.sample()
             else:
                 #Determin Action to Take
-                action = self.policy(self.epsilon, self.state_buffer)
+                #action = self.policy(self.epsilon, self.state_buffer[1:5])
+                action = self.policy(self.epsilon, self.state_buffer[:, :, 1:])
 
             # Play the Action on the environment and get the return values
             observation, reward, done, info = self.env.step(action)
             # Resize Observation
             resized = self.resize_observation(observation)
-            self.state_buffer.append(resized)
+            self.state_buffer = np.append(self.state_buffer[:, :, 1:], resized, axis=2)
+            #self.state_buffer.append(resized)
             self.reward_buffer.append(reward)
+            self.action_buffer.append(action)
             self.stepcounter += 1
             score += reward
 
@@ -145,20 +156,20 @@ class Agent():
 
             # Add Values to the Sample Buffer
             if len(self.state_buffer) >= 5:
-                state = np.moveaxis(np.asarray(self.state_buffer), [0], [2])
-                state = state.reshape(-1, state.shape[0], state.shape[1], state.shape[2])
+                state = self.state_buffer.reshape(-1, self.shape_frame[0], self.shape_frame[1], self.num_frames + 1)
                 self.sample_buffer.add_experience(state[:, :, :, :4],       # Add State(t)
+                                                  self.action_buffer[0],  # Add action(t)
+                                                  self.reward_buffer[1],  # Add reward(t+1)
                                                   state[:, :, :, 1:5],      # Add State(t+1)
-                                                  action,                   # Add action(t)
-                                                  self.reward_buffer[1],    # Add reward(t+1)
                                                   done)                     # Done Flag
 
             # If we have enough entries in the sample buffer --> learn
-            if self.sample_buffer.get_buffer_length() > 50:
+            if self.stepcounter >= 15000 and self.stepcounter % 50 == 0:
                 self.learn()
+                pass
 
-            # Save Weihghts to storage and update target network periodically
-            if (self.stepcounter % 25000) == 0:
+            # Save Weights to storage and update target network periodically
+            if (self.stepcounter % 10000) == 0:
                 self.save_current_weights()
 
             # If the episode is finished, recalculate epsilon and add score to score buffer
@@ -167,7 +178,11 @@ class Agent():
                 if self.epsilon < self.epsilon_min:
                     self.epsilon = self.epsilon_min
                 self.scores.append(score)
+                #print("Len Buffer: " +str(self.sample_buffer.get_buffer_length()))
                 #self.learn()
+                #plt.figure()
+                #plt.imshow(self.state_buffer[0])
+                #plt.show()
                 break
 
             if reward != 0.0:
@@ -178,12 +193,11 @@ class Agent():
         Learn from the action´s we´ve taken so far
         """
         # Get a Batch of the sample buffer
-        states, next_states, actions, next_rewards, dones = self.sample_buffer.get_batch()
+        states, next_states, actions, next_rewards, dones = self.sample_buffer.get_batch(600)
         # Calculate the QValues for the next_states from online model and target model
-        QValues = self.online_model.predict(tf.convert_to_tensor(np.array(next_states), dtype=tf.float32))
-        next_QValues = self.target_model.predict(tf.convert_to_tensor(np.array(next_states), dtype=tf.float32))
-        # Create Copy of QValues where the reccalculated QValues are stored
-        updated_QValues = QValues.copy()
+        QValues = self.online_model.predict(next_states) # states(t+1)
+        next_QValues = self.target_model.predict(next_states)
+        updated_QValues = self.online_model.predict(states)
 
         #Iterate through every state
         for i in range(len(QValues)):
@@ -192,18 +206,22 @@ class Agent():
             next_reward = next_rewards[i]
             # Determine Action to take of state(t+1), highest QValue of online Model at State (t+1)
             action = np.where(QValues[i] == np.amax(QValues[i]))[0][0]
-            updated_QValues[i][action] = (self.gamma * next_QValues[i][action]) + next_reward
+            nextValue = (self.gamma * next_QValues[i][action])
+            updated_QValues[i][actions[i]] = (self.gamma * next_QValues[i][action]) + next_reward
             #print("Old QValue: " + str(QValues[i][action]) + " QValue: " + str(updated_QValues[i][action]) + " Reward: " + str(next_reward) + " Action: " + str(action))
             pass
 
         # Just for debbuging
         if (self.stepcounter % 5000) == 0:
-            print("Current QValues at stepcounter " + str(self.stepcounter) + ": " + str(updated_QValues[-1]))
+            print("Current QValues at Stepcounter: " + str(self.stepcounter) + ": " + str(updated_QValues[-1]))
+            print("Current Weights at Stepcounter: " + str(self.stepcounter) + ": " + str(self.online_model.get_weights()[9]))
 
+        # pre_weights = self.online_model.get_weights()
+        # print("Pre Weights: " +str(pre_weights[9]))
         # Fit the Model with the updated_QValues and the States(t)
-        self.online_model.fit(x=tf.convert_to_tensor(np.array(states), dtype=tf.float32),
-                              y=updated_QValues,
-                              verbose=0)
+        self.online_model.fit(x=states, y=updated_QValues)
+        #past_weights = self.online_model.get_weights()
+        #print("Past Weights: " +str(past_weights[9]))
 
     def play(self):
         """
@@ -236,7 +254,10 @@ class Agent():
         print(str(datetime.datetime.now().replace(microsecond=0)) + "Finished Training!")
 
     def save_current_weights(self):
-        print(str(datetime.datetime.now().replace(microsecond=0)) + "Update Target Model and saved current weights!")
+        """
+        Function to Update the Target Model and Save the current weights to storage
+        """
+        print(str(datetime.datetime.now().replace(microsecond=0)) + " Update Target Model and saved current weights!")
         self.target_model.set_weights(self.online_model.get_weights())
         self.online_model.save_weights(self.Weights_Path)
 
--- SampleBuffer.py
+++ SampleBuffer.py
@@ -3,32 +3,42 @@ import numpy as np
 import itertools
 
 class SampleBuffer():
-    def __init__(self, buffer_size=15000):
+    def __init__(self, buffer_size=15000, shape=(84,84), num_frames=4):
         """
         Initializing a SampleBuffer for expierence Replay
         :param buffer_size: Determines the Buffer size which is choosen
         """
+        self.size = buffer_size
+        self.current = 0  # index to write to
 
-        self.states = deque(maxlen=buffer_size)
-        self.next_states = deque(maxlen=buffer_size)
-        self.actions = deque(maxlen=buffer_size)
-        self.next_rewards = deque(maxlen=buffer_size)
-        self.dones = deque(maxlen=buffer_size)
+        self.states = np.empty((self.size, shape[0], shape[1], num_frames), dtype=np.float32)
+        self.actions = np.empty(self.size, dtype=np.int32)
+        self.next_rewards = np.empty(self.size, dtype=np.float32)
+        self.next_states = np.empty((self.size, shape[0], shape[1], num_frames), dtype=np.float32)
+        self.dones = np.empty(self.size, dtype=np.int32)
+        #self.states = deque(maxlen=buffer_size)
+        #self.next_states = deque(maxlen=buffer_size)
+        #self.actions = deque(maxlen=buffer_size)
+        #self.next_rewards = deque(maxlen=buffer_size)
+        #self.dones = deque(maxlen=buffer_size)
 
-    def add_experience(self, state, next_states, actions, next_reward, done):
+
+    def add_experience(self, state,  actions, next_reward, next_states, done):
         """
         Adds an Experience to the buffer
-        :param state: Input of Model at time t
-        :param next_states: Input of Model at time t+1
-        :param actions: Action of Model at time t+1
-        :param next_reward: Reward of Model at time t+1
+        :param state: State at time t
+        :param actions: Action at time t
+        :param next_reward: Reward at time t+1
+        :param next_states: Sate at time t+1
         :param done: Done Flag at time t+1
         """
-        self.states.append(state)
-        self.next_states.append(next_states)
-        self.actions.append(actions)
-        self.next_rewards.append(next_reward)
-        self.dones.append(done)
+        self.states[self.current, ...] = state
+        self.actions[self.current] = actions
+        self.next_rewards[self.current] = next_reward
+        self.next_states[self.current, ...] = next_states
+        self.dones[self.current] = done
+        self.current = (self.current + 1) % self.size
+
 
     def get_batch(self, batchsize = 50):
         """
@@ -38,11 +48,11 @@ class SampleBuffer():
         """
         index = np.random.randint(batchsize, len(self.states))
         lower_limit = index-batchsize
-        states = list(itertools.islice(self.states, index-batchsize, index))
-        next_states = list(itertools.islice(self.next_states, index - batchsize, index))
-        actions = list(itertools.islice(self.actions, index - batchsize, index))
-        next_rewards = list(itertools.islice(self.next_rewards, index - batchsize, index))
-        dones = list(itertools.islice(self.dones, index - batchsize, index))
+        states = self.states[index-batchsize:index, :, :, :]
+        next_states = self.next_states[index-batchsize:index, :, :, :]
+        actions = self.actions[index-batchsize:index]
+        next_rewards = self.next_rewards[index-batchsize:index]
+        dones = self.dones[index-batchsize:index]
 
         return states, next_states, actions, next_rewards, dones
 
